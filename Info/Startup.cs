50f582d6e3be544bcd83c0c3ffeec8b9770d0c88
Albert <bubokastor@mail.ru>
2017-04-19 01:24:54 +0300

    fix

--- Info/Startup.cs
+++ Info/Startup.cs
@@ -30,7 +30,7 @@ namespace Info
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<InfoContext>(options  => 
+            services.AddDbContext<InfoContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc();
