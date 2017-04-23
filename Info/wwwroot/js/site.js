904e9f2cd001497860d69ce26583d044cc08c2cf
Albert <bubokastor@mail.ru>
2017-04-24 01:26:58 +0300

    add filter

--- Info/wwwroot/js/site.js
+++ Info/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿// Write your Javascript code.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var reset = function () {
+        window.location = "/";
+    }
+    changeLocation = function (location) {
+        if (window.location.href.split(location).length == 2) {
+            reset();
+        }
+        else {
+            window.location = "/?platform=" + location;
+        }
+    }
+    var query = window.location.search.split("?platform=");
+    if (query.length == 2) {
+        var button = document.getElementById("filter-" + query[1]);
+        button.classList.add("active");
+    }1
+});
